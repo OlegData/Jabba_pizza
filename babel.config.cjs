30309735f1d7c6ad08767bf5f9bcfdf95a9fc8df
Oleg <68560499+OlegData@users.noreply.github.com>
2026-03-13 09:31:52 +0100

    Update config

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,5 +1,7 @@
 module.exports = {
   presets: [
+    // Enable parsing TypeScript syntax in tests (babel-jest)
+    ["@babel/preset-typescript"],
     ["@babel/preset-env", { targets: { node: "current" } }],
     ["@babel/preset-react", { runtime: "automatic" }],
   ],
